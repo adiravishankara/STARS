971f3c865e22554ee784a008c6609193df18971b
arka1995 <arka_999@hotmail.com>
2018-04-12 11:42:36 -0700

    change2

--- GUI/guiTest.py
+++ GUI/guiTest.py
@@ -3,4 +3,10 @@ root = Tkinter.Tk()
 root.title("Testing Tkinter")
 
 print "This is where things will go"
+text  = Tkinter.Text(root, width=20, height=5, highlightthickness=2)
+	
+text.bind('<KeyPress>', reportEvent)
+	
+text.pack(expand=1, fill="both")
+text.focus_set()
 root.mainloop()
