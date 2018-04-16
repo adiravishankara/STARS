f084bdf60fb4120fd3d6d3468610fca1d3888894
arka1995 <arka_999@hotmail.com>
2018-04-15 20:50:09 -0700

    osc_test

---
+++ osc_test.py
@@ -0,0 +1,12 @@
+import signal
+import flicklib
+from time import sleep
+from curses import wrapper
+from os import system
+from OSC import OSCClient, OSCMessage
+
+client = OSCClient()
+client.connect((192.168.0.15,4559))
+
+
+client.send( OSCMessage("HI",[1]))
