c2cd32fe2bec4513d600125f6ccbde1b2114c85b
arka1995 <arka_999@hotmail.com>
2018-04-16 22:53:52 -0700

    new changes

--- GUI/guiTest.py
+++ GUI/guiTest.py
@@ -1,16 +1,31 @@
-import Tkinter
-root = Tkinter.Tk()
+import tkinter
+from tkinter import *
+from tkinter import ttk
+root = tkinter.Tk()
 root.title("Testing Tkinter")
 
-<<<<<<< HEAD
-print ("This is where things will go")
-=======
-print "This is where things will go"
-text  = Tkinter.Text(root, width=20, height=5, highlightthickness=2)
-	
-text.bind('<KeyPress>', reportEvent)
-	
-text.pack(expand=1, fill="both")
-text.focus_set()
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+#print("This is where things will go")
+
+feet = StringVar()
+meters = StringVar()
+
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind('<Return>', calculate)
+
 root.mainloop()
-this is not real
