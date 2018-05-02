e6ba5c11d76a9dcb04b6f8364138f585e65eb81c
Adithya Ravishankara <arka_999@hotmail.com>
2018-05-02 16:11:39 -0700

    Changed the file type

--- testing/osc_test.py
+++ testing/osc_test.py
@@ -1,15 +1,16 @@
 import signal
-import flicklib
+#import flicklib
 from time import sleep
 from curses import wrapper
 from os import system
 from OSC import OSCClient, OSCMessage
 
 client = OSCClient()
-client.connect(("192.168.0.15",4559))
+client.connect(("192.168.1.200",4559))
 
 
 client.send(OSCMessage("HI"))
 client.send(OSCMessage("POOP"))
 client.send(OSCMessage("THISISSHIT"))
 client.send(OSCMessage("NEWTOTHIS"))
+client.send(OSCMessage("I DONT LIKE THIS",[1]))
