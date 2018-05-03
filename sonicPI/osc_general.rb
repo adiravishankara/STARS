623bd0a4016ea77fb8346265c8deab6557a1d905
Adithya <arka_999@hotmail.com>
2018-05-03 00:00:42 +0000

    Changed SOnic pi

--- sonicPI/osc_general.rb
+++ sonicPI/osc_general.rb
@@ -1,4 +1,4 @@
-dir = "/home/pi/Documents/GIT/STARS/music"
+dir = "../../../home/pi/Documents/GIT/STARS/music"
 A = load_sample dir, :a
 B = load_sample dir, :b
 C = load_sample dir, :c
@@ -11,7 +11,7 @@ play_pattern_timed [55, 52, 57, 60, 65], [0.2,0.2,0.2,0.3]
 #the liveloops
 live_loop :swipe_ns do
   use_real_time
-  a = sync "/osc/north - south"
+  a = sync "/osc/a/north - south"
   sample A
 end
 
@@ -19,7 +19,7 @@ end
 
 live_loop :swipe_sn do
   use_real_time
-  a = sync "/osc/south - north"
+  a = sync "/osc/a/south - north"
   sample B
 end
 
@@ -27,7 +27,7 @@ end
 
 live_loop :swipe_we do
   use_real_time
-  a = sync "/osc/west - east"
+  a = sync "/osc/a/west - east"
   sample B
 end
 
@@ -35,7 +35,7 @@ end
 
 live_loop :swipe_ew do
   use_real_time
-  a, b, c = sync "/osc/east - west"
+  a = sync "/osc/a/east - west"
   sample C
   
 end
@@ -44,7 +44,7 @@ end
 
 live_loop :aw do
   use_real_time
-  a = sync "/osc/airwheel"
+  a = sync "/osc/a/airwheeltxt"
   synth :dark_ambience, note: a, sustain: 3
   
 end
@@ -53,7 +53,7 @@ end
 
 live_loop :x_range do
   use_real_time
-  a,b,c = sync "/osc/xyz"
+  a,b,c = sync "/osc/a/xyz"
   synth :piano, note: a
   
 end
@@ -62,7 +62,7 @@ end
 
 live_loop :y_range do
   use_real_time
-  a,b,c = sync "/osc/xyz"
+  a,b,c = sync "/osc/a/xyz"
   synth :dsaw, note: b
   
 end
@@ -70,7 +70,145 @@ end
 
 live_loop :z_range do
   use_real_time
-  a,b,c = sync "/osc/xyz"
+  a,b,c = sync "/osc/a/xyz"
+  synth :blade, note: c
+  
+end
+
+use_synth :pluck
+play_pattern_timed [55, 52, 57, 60, 65], [0.2,0.2,0.2,0.3]
+#the liveloops
+live_loop :swipe_ns do
+  use_real_time
+  a = sync "/osc/b/north - south"
+  sample A
+end
+
+
+
+live_loop :swipe_sn do
+  use_real_time
+  a = sync "/osc/b/south - north"
+  sample B
+end
+
+
+
+live_loop :swipe_we do
+  use_real_time
+  a = sync "/osc/b/west - east"
+  sample B
+end
+
+
+
+live_loop :swipe_ew do
+  use_real_time
+  a = sync "/osc/b/east - west"
+  sample C
+  
+end
+
+
+
+live_loop :aw do
+  use_real_time
+  a = sync "/osc/b/airwheeltxt"
+  synth :dark_ambience, note: a, sustain: 3
+  
+end
+
+
+
+live_loop :x_range do
+  use_real_time
+  a,b,c = sync "/osc/b/xyz"
+  synth :piano, note: a
+  
+end
+
+
+
+live_loop :y_range do
+  use_real_time
+  a,b,c = sync "/osc/b/xyz"
+  synth :dsaw, note: b
+  
+end
+
+
+live_loop :z_range do
+  use_real_time
+  a,b,c = sync "/osc/b/xyz"
+  synth :blade, note: c
+  
+end
+
+use_synth :pluck
+play_pattern_timed [55, 52, 57, 60, 65], [0.2,0.2,0.2,0.3]
+#the liveloops
+live_loop :swipe_ns do
+  use_real_time
+  a = sync "/osc/c/north - south"
+  sample A
+end
+
+
+
+live_loop :swipe_sn do
+  use_real_time
+  a = sync "/osc/c/south - north"
+  sample B
+end
+
+
+
+live_loop :swipe_we do
+  use_real_time
+  a = sync "/osc/c/west - east"
+  sample B
+end
+
+
+
+live_loop :swipe_ew do
+  use_real_time
+  a = sync "/osc/c/east - west"
+  sample C
+  
+end
+
+
+
+live_loop :aw do
+  use_real_time
+  a = sync "/osc/c/airwheeltxt"
+  synth :dark_ambience, note: a, sustain: 3
+  
+end
+
+
+
+live_loop :x_range do
+  use_real_time
+  a,b,c = sync "/osc/c/xyz"
+  synth :piano, note: a
+  
+end
+
+
+
+live_loop :y_range do
+  use_real_time
+  a,b,c = sync "/osc/c/xyz"
+  synth :dsaw, note: b
+  
+end
+
+
+live_loop :z_range do
+  use_real_time
+  a,b,c = sync "/osc/c/xyz"
   synth :blade, note: c
   
 end
